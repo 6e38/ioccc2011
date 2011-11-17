72460b8724d531a41ce6bcc093c24d288766ac96
Nathan <nate@floorsix.com>
2011-11-17 07:28:56 -0800

    improved character output

--- jenne.c
+++ jenne.c
@@ -18,32 +18,35 @@ D*a="                                   	 			                  		        		     
 D *b;
 I k;
 
-void s(D *p)
+void u()
 {
    struct timeval tv;
-   I i;
-   I j;
+   tv.tv_sec = 0;
+   tv.tv_usec = 40000;
+   select(0, 0, 0, 0, &tv);
+}
+
+void s(D *p)
+{
+   I i, j;
    D c;
    O (; *p != '\0'; ++p)
    {
       c = *p - 32;
       if (c > 63)
          c -= 32;
-      if (c >= 0 && c < 64)
+      if (c >= 0 && c < 59)
       {
-         O (i = 0; i < 5; i++)
+         O (i = c ? 0 : 4; i < 5; i++)
          {
-            F(" ");
+            F("  ");
             O (j = 0; j < 5; j++)
-            {
-               a[c * 25 + i * 5 + j] == ' ' ? F("  ") : F("##");
-            }
+               F(a[c * 25 + i * 5 + j] == ' ' ? "  " : "##");
             F("\n");
+            u();
          }
          F("\n");
-         tv.tv_sec = 0;
-         tv.tv_usec = 250000;
-         select(0, 0, 0, 0, &tv);
+         u();
       }
    }
 }
@@ -71,30 +74,30 @@ I g(D *h, D *q)
    I r;
 
    o = gethostbyname(h);
-   if (o == 0) R 2;
+   if (o == 0) R 4;
 
    a.sin_family = 2;
    a.sin_addr.s_addr = *((unsigned I*)o->h_addr_list[0]);
    a.sin_port = 20480;
-   if (connect(k, (struct sockaddr*)&a, sizeof(a))) R 3;
+   if (connect(k, (struct sockaddr*)&a, sizeof(a))) R 5;
 
    sprintf(b, "GET %s HTTP/1.1"N"Host: %s"N N, q, h);
    write(k, b, strlen(b));
 
    O (;i < B && !(i >= 4 && !memcmp(b + i - 4, N N, 4));)
    {
-      if (E(k, b + i++, 1) < 1) R 4;
+      if (E(k, b + i++, 1) < 1) R 6;
    }
 
    p = strstr(b, "Content-Length: ");
    if (p != 0)
    {
       n = atoi(p + 16);
-      if (n == 0) R 5;
+      if (n == 0) R 7;
 
       O (;l < n;)
       {
-         if ((r = E(k, b + l, n - l)) < 1) R 6;
+         if ((r = E(k, b + l, n - l)) < 1) R 8;
          l += r;
       }
       b[l] = '\0';
@@ -104,11 +107,11 @@ I g(D *h, D *q)
       i = 0;
       O (;n = c();)
       {
-         if (n == -1) R 7;
+         if (n == -1) R 9;
          l ^= l;
          O (;l < n;)
          {
-            if ((r = E(k, b + i + l, n - l)) < 1) R 8;
+            if ((r = E(k, b + i + l, n - l)) < 1) R 10;
             l += r;
          }
          i += l;
@@ -129,16 +132,19 @@ I main(c, v)
 
    if (c != 2) G z;
 
+   r = 2;
    if (!(b = malloc(B))) G z;
 
+   r = 3;
    if (!(k = socket(2, 1, 0))) G y;
 
-   sprintf(l, "/search.atom?q=%s&rpp=1", v[1]);
-   if (r = g("search.twitter.com", l)) G x;
+   sprintf(l, "/1/statuses/user_timeline.atom?screen_name=%s", v[1]);
+   if (r = g("api.twitter.com", l)) G x;
 
-   O (o = b; p = strstr(o, "<title>");)
+   o = strstr(b, "<entry>");
+   O (; p = strstr(o, "<title>");)
    {
-      if (!(o = strstr(o, "</title>"))) { r = 9; G x; };
+      if (!(o = strstr(o, "</title>"))) { r = 11; G x; };
       *o++ = '\0';
       s(" ### ");
       s(p + 7);
