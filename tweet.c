cee6e3356a3882ff181a519a13b2120cc03238b3
Nathan <nate@floorsix.com>
2011-12-01 20:14:23 -0800

    fixed a bug made during last checkin

--- tweet.c
+++ tweet.c
@@ -30,7 +30,7 @@
     ,16);}b g(u*h,u*q){c sockaddr_in A;c hostent*O;b i=0,l=0,n,r;u*p=0;O=H.A(h);
    if(O==0)d(x)if(!(k=H .E(2,1,0)))d(x)A.sin_family=2;A.sin_addr.s_addr=*((b*)O
   ->h_addr_list[0]);A.sin_port=htons(80);if(H.I(k,(c sockaddr*)&A,sizeof(A))
- )d(w)e(f)(B,"GET %s HTTP"              "/1.1\r\nHost: %s\r\n"
+ )d(w)e(s)(B,"GET %s HTTP"              "/1.1\r\nHost: %s\r\n"
 "\r\n",q, h);write(k, B,                strlen(B));o(;i<a&&!(i
 >=4&&!memcmp(B+i-4,"\r"                 "\n\r\n",4));){if(f(k,
 B+i++,1)<1)d(w)}p=_(B,                  "Content-Length: ");if
@@ -42,7 +42,7 @@ else{i=0;o(;(n=C ());                   ){if(n==-1)d(w)l^=l;o(
 ;}b main(C,v)b C;u **                   v;{u*p,*O,l[1337 ];b r
 ,X; void w (b _) {q=0                   ;} signal (2,w),signal
 (15,w) ;if(C!=2) d(z)                   if(!(B=malloc(a)))d(z)
-e(f)(l,"/1/statuses/"                   "user_time""line.atom"
+e(s)(l,"/1/statuses/"                   "user_time""line.atom"
 "?screen_name=%s",v[1                   ]);o(;q;){if((r=g("ap"
 "i.\164\167itter.com"                   ,l)))goto y;o(X=time(0
 );q&&time(0)-X <60;){                   if(!(O=_(B,"<en""try>"
